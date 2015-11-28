2b7e9b4c874243eb6f3d57378afea99dda3ed09a
kevin steve <qqwwee2006@gmail.com>
2015-11-28 12:48:49 +0800

    add php

---
+++ header.php
@@ -0,0 +1,34 @@
+<head>
+  <meta charset="utf-8" />
+  <title>新竹市政府</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+  <meta content="" name="description" />
+  <meta content="" name="author" />
+  <!-- BEGIN GLOBAL MANDATORY STYLES -->
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
+  <link href="assets/plugins/socicon/socicon.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/bootstrap-social/bootstrap-social.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/animate/animate.min.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <!-- END GLOBAL MANDATORY STYLES -->
+  <!-- BEGIN: BASE PLUGINS  -->
+  <link href="assets/plugins/revo-slider/css/settings.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/cubeportfolio/css/cubeportfolio.min.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/owl-carousel/owl.transitions.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css" />
+  <link href="assets/plugins/slider-for-bootstrap/css/slider.css" rel="stylesheet" type="text/css" />
+  <!-- END: BASE PLUGINS -->
+  <!-- BEGIN THEME STYLES -->
+  <link href="assets/base/css/plugins.css" rel="stylesheet" type="text/css" />
+  <link href="assets/base/css/components.css" id="style_components" rel="stylesheet" type="text/css" />
+  <link href="assets/base/css/themes/default.css" rel="stylesheet" id="style_theme" type="text/css" />
+  <link href="assets/base/css/custom.css" rel="stylesheet" type="text/css" />
+  <!-- END THEME STYLES -->
+  <link rel="shortcut icon" href="favicon.ico" /> 
+</head>
